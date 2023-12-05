b0b918aff72edc61675bc25f04cb5aa74fb27f25
Konrad Sitek <69002597+sit3kk@users.noreply.github.com>
2023-12-05 16:04:46 +0100

    Solved

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Student1 on 05/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
