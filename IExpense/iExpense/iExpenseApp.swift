c43a66e4cd75074372b06d9a3b381a95049416fa
Konrad Sitek <69002597+sit3kk@users.noreply.github.com>
2024-01-08 14:27:01 +0100

    Add files via upload

---
+++ IExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Konrad Sitek on 08/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
