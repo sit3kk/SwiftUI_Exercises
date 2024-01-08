499114ffd3011e328071c2df0f51f095916d945a
Konrad Sitek <69002597+sit3kk@users.noreply.github.com>
2024-01-08 14:41:08 +0100

    Add files via upload

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Konrad Sitek on 08/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
