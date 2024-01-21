64a28bb82a7f2b4d9b75a42f4f89fed05a1b17e4
Konrad Sitek <69002597+sit3kk@users.noreply.github.com>
2024-01-21 20:36:23 +0100

    Add files via upload

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Konrad Sitek on 21/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
