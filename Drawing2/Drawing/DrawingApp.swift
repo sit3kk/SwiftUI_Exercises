dd9136a8119710a5070c0ded71dfd104cadb7037
Konrad Sitek <69002597+sit3kk@users.noreply.github.com>
2024-01-21 21:03:57 +0100

    Add files via upload

---
+++ Drawing2/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Konrad Sitek on 21/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
