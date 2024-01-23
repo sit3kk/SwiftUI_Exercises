6527bb335271d6870e7a339e8ce83aee129fac92
Konrad Sitek <69002597+sit3kk@users.noreply.github.com>
2024-01-23 14:42:35 +0100

    BetterRest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Konrad Sitek on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
